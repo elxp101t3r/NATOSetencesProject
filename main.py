13ccca6c582745709451113f96b776a46b9fbe3a
elxp101t3r <eliotx10101@outlook.com>
2024-01-23 18:27:55 +0200

    Add a fix for key errors

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ dict_of_data = {row.letter:row.code for (index, row) in df.iterrows()}
 while True:
     user_setence = input('Setence:\n')
     os.system('clear')
-    list_of_words = [dict_of_data[letter] for letter in user_setence.upper() if letter != ' ']
-    print(list_of_words)
-    time.sleep(5)
+    try:
+        list_of_words = [dict_of_data[letter] for letter in user_setence.upper() if letter != ' ']
+        print(list_of_words)
+    except KeyError:
+        print('Sorry, only letters in the alphabet please.')
+    time.sleep(len(list_of_words) + 1)
     os.system('clear')
     
     if user_setence == 'exit':
